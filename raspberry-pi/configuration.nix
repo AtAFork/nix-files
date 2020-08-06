b009c501c7818857b8f4e988283c88872282839e
Adam <AtAFork@users.noreply.github.com>
2020-08-06 10:17:34 -0500

    Update configuration.nix

--- raspberry-pi/configuration.nix
+++ raspberry-pi/configuration.nix
@@ -1,11 +1,9 @@
-{ lib, config, pkgs, ... }:
+# To build, use:
+# nix-build nixos -I nixos-config=nixos/modules/installer/cd-dvd/sd-image-raspberrypi.nix -A config.system.build.sdImage
+{ config, lib, pkgs, ... }:
 
 {
   imports = [
-    ../modules/users/brian.nix
-
-    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image-raspberrypi.nix>
-
     <nixpkgs/nixos/modules/installer/scan/detected.nix>
     <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
     <nixpkgs/nixos/modules/profiles/clone-config.nix>
@@ -14,11 +12,15 @@
 
   disabledModules = [
     <nixpkgs/nixos/modules/profiles/base.nix>
-    <nixpkgs/nixos/modules/installer/tools/auto-upgrade.nix>
     <nixpkgs/nixos/modules/tasks/auto-upgrade.nix>
     <nixpkgs/nixos/modules/profiles/installation-device.nix>
     <nixpkgs/nixos/modules/installer/tools/tools.nix>
   ];
+
+  # if you have a Raspberry Pi 2 or 3, pick this:
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+#   boot.kernelPackages = pkgs.linuxPackages_rpi1;
+
 
   nixpkgs = {
     overlays = [ (self: super: {
@@ -33,6 +35,7 @@
     localSystem = { system = "x86_64-darwin"; };
   };
 
+
   # Things that don't compile or aren't needed
   fonts.fontconfig.enable = false;
   documentation.enable = false;
@@ -40,6 +43,48 @@
   boot.supportedFilesystems = [ "vfat" ];
   services.udisks2.enable = false;
   xdg.mime.enable = false;
+
+
+  # A bunch of boot parameters needed for optimal runtime on RPi 3b+
+  boot.kernelParams = ["cma=256M"];
+  boot.loader.raspberryPi.enable = true;
+  boot.loader.raspberryPi.version = 3;
+  boot.loader.raspberryPi.uboot.enable = true;
+  boot.loader.raspberryPi.firmwareConfig = ''
+    gpu_mem=256
+  '';
+
+  environment.systemPackages = with pkgs; [
+    raspberrypi-tools
+    pkgs.tmux
+    pkgs.lynx
+  ];
+
+  # File systems configuration for using the installer's partition layout
+  fileSystems = {
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXOS_BOOT";
+      fsType = "vfat";
+    };
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  # Preserve space by sacrificing documentation and history
+  services.nixosManual.enable = false;
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 30d";
+  boot.cleanTmpDir = true;
+
+  # Configure basic SSH access
+  services.openssh.enable = true;
+  services.openssh.permitRootLogin = "yes";
+
+  # Use 2GB of additional swap memory in order to not run out of memory
+  # when installing lots of things while running other things at the same time.
+  swapDevices = [ { device = "/swapfile"; size = 2048; } ];
 
   networking.wireless = {
     enable = true;
@@ -49,10 +94,5 @@
     };
   };
 
-  services.openssh.enable = true;
 
-  environment.systemPackages = [
-    pkgs.tmux
-    pkgs.lynx
-  ];
 }
